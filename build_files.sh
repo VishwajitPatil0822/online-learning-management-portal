3eb47b2628a03421224b64131c5266f878557729
Vishwajit Patil <vishwajit22patil@gmail.com>
2025-04-29 10:15:15 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic --noinput
+mkdir -p staticfiles_build/static
+cp -r static/* staticfiles_build/static/
